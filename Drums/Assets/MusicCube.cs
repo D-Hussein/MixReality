38f7173252fcb9cb221a13164bf78ea4042e27df
ziad <ziadb.zoro@gmail.com>
2020-11-26 01:23:34 +0100

    8th attempt

---
+++ Drums/Assets/MusicCube.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class MusicCube : MonoBehaviour
+{
+
+    //Sound
+    private AudioSource source;
+    public AudioClip keySound;
+    public VideoClip videoClip;
+
+    private bool on = false;
+    private bool keyhit = false;
+    private GameObject key;
+
+    private float keyDownDist = 0.025f;
+    private float returnSpeed = 0.001f;
+    private float keyOriginalY;
+
+    private float hitAgainTime = 0.5f;
+    private float canHitAgain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        var videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        key = transform.GetChild(0).gameObject;
+        keyOriginalY = key.transform.position.y;
+        videoPlayer.playOnAwake = false;
+        videoPlayer.clip = videoClip;
+        source.GetComponent<AudioSource>().volume = 0.2f;
+        // videoPlayer.targetMaterialRenderer
+        // videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        //  videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+        //  videoPlayer.targetMaterialProperty = "_MainTex";
+        // videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        // videoPlayer.SetTargetAudioSource(0, audioSource);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vp = GetComponent<VideoPlayer>();
+        if (keyhit == true)
+        {
+
+            if (vp.isPlaying && source.isPlaying)
+            {
+                vp.Stop();
+                source.Stop();
+                keyhit = false;
+                on = false;
+
+                key.transform.position = new Vector3(key.transform.position.x, key.transform.position.y - keyDownDist, key.transform.position.z);
+
+            }
+            else
+            {
+                vp.Play();
+                source.PlayOneShot(keySound);
+                keyhit = false;
+                on = !on;
+
+                key.transform.position = new Vector3(key.transform.position.x, key.transform.position.y - keyDownDist, key.transform.position.z);
+
+            }
+
+        }
+
+
+        if (key.transform.position.y < keyOriginalY)
+        {
+            key.transform.position += new Vector3(0, returnSpeed, 0);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerHand") && canHitAgain < Time.time)
+        {
+            canHitAgain = Time.time + hitAgainTime;
+            keyhit = true;
+        }
+    }
+}
---
+++ Drums/Assets/MusicKey.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MusicKey : MonoBehaviour
+{
+
+    //Sound
+    private AudioSource source;
+    public AudioClip keySound;
+
+    private bool on = false;
+    private bool keyhit = false;
+    private GameObject key;
+
+    private float keyDownDist = 0.025f;
+    private float returnSpeed = 0.001f;
+    private float keyOriginalY;
+
+    private float hitAgainTime = 0.005f;
+    private float canHitAgain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+
+        key = transform.GetChild(0).gameObject;
+        keyOriginalY = key.transform.position.y;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(keyhit == true)
+        {
+            source.PlayOneShot(keySound);
+            keyhit = false;
+            on = !on;
+
+            key.transform.position = new Vector3(key.transform.position.x, key.transform.position.y - keyDownDist, key.transform.position.z);
+
+
+        }
+
+        if(key.transform.position.y < keyOriginalY)
+        {
+            key.transform.position += new Vector3(0, returnSpeed, 0);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PlayerFinger") && canHitAgain < Time.time)
+        {
+            canHitAgain = Time.time + hitAgainTime;
+            keyhit = true;
+        }
+    }
+}
